a525d8dac5420497690714e95bf24af0ce000855
vishwnathparthe8-byte <vishwnathparthe8@gmail.com>
2025-09-26 13:41:17 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "ecommerce.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
